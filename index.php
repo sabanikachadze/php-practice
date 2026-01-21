da67eb873896540d1f0d02e8904599c48d03de7f
Saba Nikachadze <nikachadzesaba027@gmail.com>
2026-01-21 18:25:14 +0400

    PHP data types

--- index.php
+++ index.php
@@ -1,7 +1,59 @@
-<?php 
+<?php
+declare(strict_types=1);
+#Scalar Types
+
+    # Boolean
+$completed = true;
+    # Integer
+$score = 75;
+    # Float
+$price = 0.99;
+    # String
+$greeting = "Hello Saba";
+
+echo '<pre>'; 
+
+echo $completed . '<br/>';
+var_dump($completed);
+echo '<br/>';
+
+echo $score . '<br/>';
+var_dump($score);
+echo '<br/>';
+
+echo $price . '<br/>';
+var_dump($price);
+echo '<br/>';
+
+echo $greeting . '<br/>';
+var_dump($greeting);
+echo '<br/>';
+
+echo gettype($completed);
+echo '</pre>';
 
 
-$firstName = 'Saba';
-$$firstName = 'Joe';
+#Compound types
 
-echo $$firstName;
+    # array
+$companies = [1,2,3,0.2,0.9,-0.7, true,'A' , 'b'];
+
+
+#object
+#callable
+#iterable
+
+# 2 Special Types
+
+    # resource 
+
+
+function sum($x, $y){
+    return $x + $y;
+}
+
+$sum = sum(2,3);;
+
+echo $sum . '<br />';
+
+var_dump($sum);
